9ae0905cd6fedc84e62d3e08951f0d4ffec66800
rosilan <hamza.onar07@gmail.com>
2020-05-26 20:52:32 +0300

    yedek

---
+++ posts/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+	path('post_list/', views.post_list, name='post_list'),
+	path('detail/<int:pk>/', views.post_detail, name='post_detail'),
+	path('new/', views.post_new, name='post_create'),
+	path('<int:pk>/edit/', views.post_edit, name='post_edit'),
+]
---
+++ posts/views.py
@@ -0,0 +1,55 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.utils import timezone
+from .models import Post
+from .forms import PostForm
+
+def post_list(request):
+	context = {
+		'posts': [i for i in Post.objects.all()]
+	}
+
+	return render(request, 'posts/post_list.html', context)
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'posts/post_detail.html', {'post': post})
+
+def post_new(request):
+	if request.method == "POST":
+		form = PostForm(request.POST)
+		if form.is_valid():
+			post = form.save(commit=False)
+			post.author = request.user
+			post.published_date = timezone.now()
+			post.save()
+			return redirect('post_detail', pk=post.pk) 
+	else:
+		form = PostForm()
+	return render(request, 'posts/post_create.html', {'form': form})
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == "POST":
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.published_date = timezone.now()
+            post.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
+    return render(request, 'posts/post_edit.html', {'form': form})
+
+
+
+
+
+
+
+
+
+
+
+
+
